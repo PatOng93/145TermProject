47657b7643ac43761c0befb0fda8b51a8372d5ff
andrewvt100 <andrewvt100@gmail.com>
2017-03-21 22:20:13 -0700

    fixed examples

--- examples.r
+++ examples.r
@@ -3,17 +3,17 @@
 testbintree <- function() {
 
 	mytree <- newbintree()
-	push(mytree,5,"mytree")
-	push(mytree,4,"mytree")
-	push(mytree,6,"mytree")
-	push(mytree,6,"mytree")
-	push(mytree,7,"mytree")
-	push(mytree,8,"mytree")
+	mytree <- push(mytree,5)
+	mytree <- push(mytree,4)
+	mytree <- push(mytree,6)
+	mytree <- push(mytree,6)
+	mytree <- push(mytree,7)
+	mytree <- push(mytree,8)
 
 	print(mytree$data)
 	pop(mytree, "mytree")
 	print(mytree$data)
-	push(mytree,3,"mytree")
+	push(mytree,3)
 	print(mytree$data)
 
 	element <- pop(mytree, "mytree")
@@ -25,11 +25,11 @@ testbintree <- function() {
 teststack <- function() {
 
 	mystack <- newstack()
-	push(mystack, 5, "mystack")
-	push(mystack, 10, "mystack")
-	push(mystack, 15, "mystack")
-	push(mystack, 20, "mystack")
-	push(mystack, 25, "mystack")
+	mystack <- push(mystack, 5)
+	mystack <- push(mystack, 10)
+	mystack <- push(mystack, 15)
+	mystack <- push(mystack, 20)
+	mystack <- push(mystack, 25)
 	print(mystack$data)
 	pop(mystack, "mystack")
 	print(mystack$data)
@@ -48,11 +48,11 @@ teststack <- function() {
 testqueue <- function() {
 
 	myqueue <- newqueue()
-	push(myqueue, 5, "myqueue")
-	push(myqueue, 10, "myqueue")
-	push(myqueue, 15, "myqueue")
-	push(myqueue, 20, "myqueue")
-	push(myqueue, 25, "myqueue")
+	myqueue <- push(myqueue, 5)
+	myqueue <- push(myqueue, 10)
+	myqueue <- push(myqueue, 15)
+	myqueue <- push(myqueue, 20)
+	myqueue <- push(myqueue, 25)
 	print(myqueue$data)
 	pop(myqueue, "myqueue")
 	print(myqueue$data)
